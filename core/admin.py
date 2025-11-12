baacf042ca5465bb6643639eef13154b61326233
Stephanny <stephannyborgestete@gmail.com>
2025-11-12 09:38:34 -0300

    correção no admin.py

--- core/admin.py
+++ core/admin.py
@@ -3,23 +3,9 @@ from .models import CardapioSemanal
 from .models import AdminRequest
 
 admin.site.register(CardapioSemanal)
+
+# Esta é a versão corrigida e mais completa da sua classe
 @admin.register(AdminRequest)
-class AdminRequestAdmin(admin.ModelAdmin):
-    list_display = ('user', 'aprovado', 'data_solicitacao')
-    list_filter = ('aprovado',)
-    actions = ['aprovar_admins']
-
-    def aprovar_admins(self, request, queryset):
-        for req in queryset:
-            req.aprovado = True
-            req.save()
-            user = req.user
-            user.is_active = True
-            user.is_staff = True
-            user.save()
-        self.message_user(request, "Admins aprovados com sucesso!")
-
-    @admin.register(AdminRequest)
 class AdminRequestAdmin(admin.ModelAdmin):
     list_display = ('user', 'aprovado', 'data_solicitacao', 'aprovado_por')
     list_filter = ('aprovado',)
@@ -34,4 +20,5 @@ class AdminRequestAdmin(admin.ModelAdmin):
             solicitacao.aprovado_por = request.user
             solicitacao.save()
         self.message_user(request, "Solicitações aprovadas com sucesso!")
+    
     aprovar_solicitacoes.short_description = "Aprovar solicitações selecionadas"
